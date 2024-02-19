4e99816cc9dee43b2c8d7934e22919f8c1104f4b
Johnny Huang <johnny>
2024-02-19 10:10:52 +0800

    fix(nvim): change logo back
    since the command broke vim due to mem leak

--- nvim/.config/nvim/lua/custom/configs/dashboard.lua
+++ nvim/.config/nvim/lua/custom/configs/dashboard.lua
@@ -23,13 +23,11 @@ local doom = {
     packages = { enable = true },
   },
   preview = {
-    -- command = "lolcat | lolcat",
-    command = "cmatrix -ba -C cyan",
+    command = "lolcat | lolcat",
     -- https://patorjk.com/software/taag/#p=testall&h=3&v=0&f=Big&t=Viewsonic
-    file_path = "",
-    -- file_path = home .. "/.config/nvim/lua/custom/logo/Star-Wars.txt",
-    file_height = 25,
-    file_width = 200,
+    file_path = home .. "/.config/nvim/lua/custom/logo/Star-Wars.txt",
+    file_height = 12,
+    file_width = 100,
   },
 }
 
