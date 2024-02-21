fae7bb1b61c986b8b403df9f71e39120b1be496b
Johnny Huang <johnny>
2024-02-21 15:26:56 +0800

    chore(nvim): add terraform formatter and remove alacritty navigator

--- nvim/.config/nvim/lua/custom/configs/null-ls.lua
+++ nvim/.config/nvim/lua/custom/configs/null-ls.lua
@@ -1,29 +1,30 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
 local opts = {
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.terraform_fmt,
   },
   on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds {
         group = augroup,
         buffer = bufnr,
-      })
+      }
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({
+          vim.lsp.buf.format {
             bufnr = bufnr,
             -- Never request typescript-language-server for formatting because it will conflict with prettier
             filter = function(client)
               return client.name ~= "tsserver"
             end,
-          })
+          }
         end,
       })
     end
