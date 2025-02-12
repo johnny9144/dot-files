c0867683c0b03117cbe9b7db578334c1758ef873
Johnny Huang <johnny>
2025-02-12 17:16:54 +0800

    chore(nvim): set hcl,terraform comment string

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -3,6 +3,12 @@ require "nvchad.options"
 -- add yours here!
 
 vim.opt.relativenumber = true -- Relative line numbers
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "terraform", "hcl" },
+  callback = function()
+    vim.bo.commentstring = "# %s"
+  end,
+})
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
