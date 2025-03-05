d557c630245d59d0783fffe32e6e738ab669be0c
Johnny Huang <johnny>
2025-03-05 09:35:15 +0800

    fix(nvim): spectre incorrect backup behavior after replace

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -104,5 +104,24 @@ return {
   {
     "nvim-pack/nvim-spectre",
     event = "VeryLazy",
+    config = function()
+      require("spectre").setup {
+        replace_engine = {
+          ["sed"] = {
+            cmd = "sed",
+            args = {
+              "-i",
+              "",
+              "-E",
+            },
+          },
+        },
+        default = {
+          replace = {
+            cmd = "sed",
+          },
+        },
+      }
+    end,
   },
 }
